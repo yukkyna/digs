056c74426ad8b179b828464fd2aa3a5223aa2ab4
yukkyna <yukkyna.github@gmail.com>
2013-12-29 05:47:18 +0900

    update symfony

--- src/Digs/PhotoBundle/Controller/PhotoController.php
+++ src/Digs/PhotoBundle/Controller/PhotoController.php
@@ -53,24 +53,28 @@ class PhotoController extends Controller
         ));
     }
 
-	/**
-     * Creates a new Photo entity.
+    /**
+     * Displays a form to create a new Photo entity.
      *
      */
-    public function createAction(Request $request)
+    public function newAction(Request $request)
     {
         $entity = new Photo();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
+        $form   = $this->createCreateForm($entity);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
+		if ($request->isMethod('POST'))
+		{
+			$form->handleRequest($request);
 
-            return $this->redirect($this->generateUrl('photo_show', array('id' => $entity->getId())));
-        }
+			if ($form->isValid()) {
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($entity);
+				$em->flush();
 
+				return $this->redirect($this->generateUrl('photo_show', array('id' => $entity->getId())));
+			}
+		}
+		
         return $this->render('DigsPhotoBundle:Photo:new.html.twig', array(
             'entity' => $entity,
             'form'   => $form->createView(),
@@ -87,7 +91,7 @@ class PhotoController extends Controller
     private function createCreateForm(Photo $entity)
     {
         $form = $this->createForm(new PhotoType(), $entity, array(
-            'action' => $this->generateUrl('photo_create'),
+            'action' => $this->generateUrl('photo_new'),
             'method' => 'POST',
         ));
 
@@ -96,22 +100,7 @@ class PhotoController extends Controller
         return $form;
     }
 
-    /**
-     * Displays a form to create a new Photo entity.
-     *
-     */
-    public function newAction()
-    {
-        $entity = new Photo();
-        $form   = $this->createCreateForm($entity);
-
-        return $this->render('DigsPhotoBundle:Photo:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
-
-    /**
+	/**
      * Finds and displays a Photo entity.
      *
      */
@@ -182,79 +171,79 @@ class PhotoController extends Controller
 //            'entity'      => $entity,
 //			));
     }
-
-	/**
-     * Displays a form to edit an existing Photo entity.
-     *
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('DigsPhotoBundle:Photo')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Photo entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('DigsPhotoBundle:Photo:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-    * Creates a form to edit a Photo entity.
-    *
-    * @param Photo $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Photo $entity)
-    {
-        $form = $this->createForm(new PhotoType(), $entity, array(
-            'action' => $this->generateUrl('photo_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-    /**
-     * Edits an existing Photo entity.
-     *
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('DigsPhotoBundle:Photo')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Photo entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('photo_edit', array('id' => $id)));
-        }
-
-        return $this->render('DigsPhotoBundle:Photo:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
+//
+//	/**
+//     * Displays a form to edit an existing Photo entity.
+//     *
+//     */
+//    public function editAction($id)
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $entity = $em->getRepository('DigsPhotoBundle:Photo')->find($id);
+//
+//        if (!$entity) {
+//            throw $this->createNotFoundException('Unable to find Photo entity.');
+//        }
+//
+//        $editForm = $this->createEditForm($entity);
+//        $deleteForm = $this->createDeleteForm($id);
+//
+//        return $this->render('DigsPhotoBundle:Photo:edit.html.twig', array(
+//            'entity'      => $entity,
+//            'edit_form'   => $editForm->createView(),
+//            'delete_form' => $deleteForm->createView(),
+//        ));
+//    }
+//
+//    /**
+//    * Creates a form to edit a Photo entity.
+//    *
+//    * @param Photo $entity The entity
+//    *
+//    * @return \Symfony\Component\Form\Form The form
+//    */
+//    private function createEditForm(Photo $entity)
+//    {
+//        $form = $this->createForm(new PhotoType(), $entity, array(
+//            'action' => $this->generateUrl('photo_update', array('id' => $entity->getId())),
+//            'method' => 'PUT',
+//        ));
+//
+//        $form->add('submit', 'submit', array('label' => 'Update'));
+//
+//        return $form;
+//    }
+//    /**
+//     * Edits an existing Photo entity.
+//     *
+//     */
+//    public function updateAction(Request $request, $id)
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $entity = $em->getRepository('DigsPhotoBundle:Photo')->find($id);
+//
+//        if (!$entity) {
+//            throw $this->createNotFoundException('Unable to find Photo entity.');
+//        }
+//
+//        $deleteForm = $this->createDeleteForm($id);
+//        $editForm = $this->createEditForm($entity);
+//        $editForm->handleRequest($request);
+//
+//        if ($editForm->isValid()) {
+//            $em->flush();
+//
+//            return $this->redirect($this->generateUrl('photo_edit', array('id' => $id)));
+//        }
+//
+//        return $this->render('DigsPhotoBundle:Photo:edit.html.twig', array(
+//            'entity'      => $entity,
+//            'edit_form'   => $editForm->createView(),
+//            'delete_form' => $deleteForm->createView(),
+//        ));
+//    }
     /**
      * Deletes a Photo entity.
      *
